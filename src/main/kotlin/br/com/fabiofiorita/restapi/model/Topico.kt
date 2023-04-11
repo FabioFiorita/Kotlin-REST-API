d2fbf4932a08515759fa2d2e0cd1ff6175263aa4
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-11 14:36:45 -0300

    feat: updating topico data class

--- src/main/kotlin/br/com/fabiofiorita/restapi/model/Topico.kt
+++ src/main/kotlin/br/com/fabiofiorita/restapi/model/Topico.kt
@@ -1,4 +1,14 @@
 package br.com.fabiofiorita.restapi.model
 
-data class Topico() {
-}
+import java.time.LocalDateTime
+
+data class Topico(
+    var id: Long? = null,
+    val titulo: String,
+    val mensagem: String,
+    val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    val curso: Curso,
+    val autor: Usuario,
+    val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
+    val respostas: List<Resposta> = ArrayList()
+)
