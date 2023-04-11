307dd2a5b74084974d4f001194db7c90ec1a064e
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-11 14:39:01 -0300

    feat: creating Topico service class

---
+++ src/main/kotlin/br/com/fabiofiorita/restapi/service/TopicoService.kt
@@ -0,0 +1,35 @@
+package br.com.fabiofiorita.restapi.service
+
+import br.com.fabiofiorita.restapi.dto.TopicoForm
+import br.com.fabiofiorita.restapi.dto.TopicoView
+import br.com.fabiofiorita.restapi.mapper.TopicoFormMapper
+import br.com.fabiofiorita.restapi.mapper.TopicoViewMapper
+import br.com.fabiofiorita.restapi.model.Topico
+import org.springframework.stereotype.Service
+import java.util.stream.Collectors
+
+@Service
+class TopicoService(
+    private var topicos: List<Topico> = ArrayList(),
+    private val topicoViewMapper: TopicoViewMapper,
+    private val topicoFormMapper: TopicoFormMapper
+) {
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map { t ->
+            topicoViewMapper.map(t)
+        }.collect(Collectors.toList())
+    }
+
+    fun buscarPorId(id: Long): TopicoView {
+        val topico = topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+        return topicoViewMapper.map(topico)
+    }
+
+    fun cadastrar(form: TopicoForm) {
+        val topico = topicoFormMapper.map(form)
+        topico.id = topicos.size.toLong() + 1
+        topicos = topicos.plus(topico)
+    }
+}
