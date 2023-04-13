d153928e81c62994534e1e407c14c899788ced90
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-13 20:35:19 -0300

    feat: adding persistance to models

--- src/main/kotlin/br/com/fabiofiorita/restapi/model/Resposta.kt
+++ src/main/kotlin/br/com/fabiofiorita/restapi/model/Resposta.kt
@@ -1,12 +1,19 @@
 package br.com.fabiofiorita.restapi.model
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
 import java.time.LocalDateTime
-
+@Entity
 data class Resposta (
+    @Id @GeneratedValue
     val id: Long? = null,
     val mensagem: String,
     val dataCriacao: LocalDateTime = LocalDateTime.now(),
+    @ManyToOne
     val autor: Usuario,
+    @ManyToOne
     val topico: Topico,
     val solucao: Boolean
 )
--- src/main/kotlin/br/com/fabiofiorita/restapi/model/Usuario.kt
+++ src/main/kotlin/br/com/fabiofiorita/restapi/model/Usuario.kt
@@ -1,6 +1,12 @@
 package br.com.fabiofiorita.restapi.model
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+
+@Entity
 data class Usuario(
+    @Id @GeneratedValue
     val id: Long? = null,
     val nome: String,
     val email: String
