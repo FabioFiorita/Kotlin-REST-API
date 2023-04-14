8db53a4a8d3673b4a0dcc9dfdc1dda41cf5fc4d3
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-14 14:23:17 -0300

    feat: creating sql security commands

--- src/main/resources/db/migration/V8__create_table_usuario_role.sql
+++ src/main/resources/db/migration/V8__create_table_usuario_role.sql
@@ -6,3 +6,5 @@ create table usuario_role(
     foreign key (usuario_id) references usuario(id),
     foreign key (role_id) references role(id)
 );
+
+insert into usuario_role(id, usuario_id, role_id) values (1, 1, 1);
