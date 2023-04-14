7384ec5a408f60bd8e3a5d2ed423db98cd9fdc98
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-14 14:04:09 -0300

    feat: adding role to user

--- src/main/kotlin/br/com/fabiofiorita/restapi/model/Usuario.kt
+++ src/main/kotlin/br/com/fabiofiorita/restapi/model/Usuario.kt
@@ -1,14 +1,18 @@
 package br.com.fabiofiorita.restapi.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.*
 
 @Entity
 data class Usuario(
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val nome: String,
     val email: String,
-    val password: String
+    val password: String,
+
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "usuario_role")
+    val roles: List<Role> = mutableListOf()
 )
