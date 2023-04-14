d66ff420199888e9ae52b506abf8ae09f758225f
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-14 10:33:50 -0300

    feat: adding function to TopicoRepository

--- src/main/kotlin/br/com/fabiofiorita/restapi/repository/TopicoRepository.kt
+++ src/main/kotlin/br/com/fabiofiorita/restapi/repository/TopicoRepository.kt
@@ -1,7 +1,10 @@
 package br.com.fabiofiorita.restapi.repository
 
 import br.com.fabiofiorita.restapi.model.Topico
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface TopicoRepository: JpaRepository<Topico, Long> {
+    fun findByCursoNome(nomeCurso: String, paginacao: Pageable): Page<Topico>
 }
