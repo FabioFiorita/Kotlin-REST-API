b01eccc9ad720464170c0d5cf7f21ea0b45cc412
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-14 14:23:02 -0300

    feat: refactoring

--- src/main/kotlin/br/com/fabiofiorita/restapi/model/Role.kt
+++ src/main/kotlin/br/com/fabiofiorita/restapi/model/Role.kt
@@ -3,14 +3,14 @@ package br.com.fabiofiorita.restapi.model
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
-import org.springframework.data.annotation.Id
+import jakarta.persistence.Id
 import org.springframework.security.core.GrantedAuthority
 
 @Entity
 data class Role(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private val id: Long,
-    private val nome: String
+    val id: Long,
+    val nome: String
 ) : GrantedAuthority {
     override fun getAuthority() = nome
 }
