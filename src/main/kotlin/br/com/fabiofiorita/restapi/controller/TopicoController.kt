c53fd24ffcb5f7581a85548cdbe79513deabd4d1
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-14 10:34:38 -0300

    feat: adding requestParam, page size and sort to listar

--- src/main/kotlin/br/com/fabiofiorita/restapi/controller/TopicoController.kt
+++ src/main/kotlin/br/com/fabiofiorita/restapi/controller/TopicoController.kt
@@ -6,6 +6,10 @@ import br.com.fabiofiorita.restapi.dto.TopicoView
 import br.com.fabiofiorita.restapi.service.TopicoService
 import jakarta.transaction.Transactional
 import jakarta.validation.Valid
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -16,8 +20,11 @@ import java.util.*
 @RequestMapping("/topicos")
 class TopicoController(private val service: TopicoService) {
     @GetMapping
-    fun listar(): List<TopicoView> {
-        return service.listar()
+    fun listar(
+        @RequestParam(required = false) nomeCurso: String?,
+        @PageableDefault(size = 5, sort = ["dataCriacao"], direction = Sort.Direction.DESC) paginacao: Pageable
+    ): Page<TopicoView> {
+        return service.listar(nomeCurso = nomeCurso, paginacao = paginacao)
     }
 
     @GetMapping("/{id}")
