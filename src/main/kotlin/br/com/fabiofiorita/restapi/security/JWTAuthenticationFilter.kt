8f22704ce68912c3f8aec2b34ca7540b73519f05
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-15 14:43:38 -0300

    fix: fixing auth

--- src/main/kotlin/br/com/fabiofiorita/restapi/security/JWTAuthenticationFilter.kt
+++ src/main/kotlin/br/com/fabiofiorita/restapi/security/JWTAuthenticationFilter.kt
@@ -4,20 +4,28 @@ import br.com.fabiofiorita.restapi.config.JWTUtil
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
 class JWTAuthenticationFilter(private val jwtUtil: JWTUtil) : OncePerRequestFilter() {
-
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
-        val token = request.getHeader("Authorization").replace("Bearer ", "")
-
-        if(jwtUtil.isValid(token)) {
-            val authentication: Authentication = jwtUtil.getAuthentication(token)
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = request.getHeader("Authorization")
+        val jwt = getTokenDetail(token) ?: ""
+        if (jwtUtil.isValid(jwt)) {
+            val authentication = jwtUtil.getAuthentication(jwt)
             SecurityContextHolder.getContext().authentication = authentication
         }
-
         filterChain.doFilter(request, response)
+    }
+
+    private fun getTokenDetail(token: String?): String? {
+        return token?.let {jwt ->
+            jwt.startsWith("Bearer ")
+            jwt.substring(7, jwt.length)
+        }
     }
 }
